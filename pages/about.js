2a49b088cdbf028e91380af4085dce9f1f927cba
kq6032 <kevin.z.q77@gmail.com>
2022-07-19 15:52:07 -0700

    Fixed apostrophe error in About page html

--- pages/about.js
+++ pages/about.js
@@ -10,8 +10,8 @@ export default function About() {
                 <div className="bg-gray-900 -mt-10 min-h-screen">
                     <div className="text-container max-w-6xl mx-auto px-4 pt-32">
                         <div className="leading-loose text-2xl font-normal text-slate-300 my">
-                            <p>Hey, Nice to meet you! I'm Kevin!</p>
-                            <p>Currently, I'm an undergraduate student at Columbia University majoring in Computer Engineering.</p>
+                            <p>Hey, Nice to meet you! I&#39;m Kevin!</p>
+                            <p>Currently, I&#39;m an undergraduate student at Columbia University majoring in Computer Engineering.</p>
                         </div>
                     </div>
                 </div>
